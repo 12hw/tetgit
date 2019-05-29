68a52a05a928049485a6201f2e8c46a222d29920
stronghwan <stronghwan@163.com>
2019-05-29 11:31:30 +0800

    git test

---
+++ src/main/java/cn/stronghwan/tetgit/TetgitApplication.java
@@ -0,0 +1,13 @@
+package cn.stronghwan.tetgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TetgitApplication.class, args);
+    }
+
+}
